9f6fafce2a7ddff231171b9d06bfa883e4d310f4
le-garden-fox <le-garden-fox@example.com>
2020-09-13 03:45:22 -0700

    Removed hooks

--- hooks/post_gen_project.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash 
-
-
-go mod init {{cookiecutter.repository}}/{{cookiecutter.repository_user}}/{{cookiecutter.module_name}}
--- {{cookiecutter.project_name}}/cmd.sh
+++ {{cookiecutter.project_name}}/cmd.sh
@@ -3,7 +3,7 @@
 function main() {
     case $1 in
     "gen")
-        protoc {{cookiecutter.module_name}}/{{cookiecutter.module_name}}-pb/{{cookiecutter.module_name}}.proto --go_out=plugins=grpc:.
+        protoc {{cookiecutter.module_name}}/{{cookiecutter.module_name}}pb/{{cookiecutter.module_name}}.proto --go_out=plugins=grpc:.
         ;;
     *)
         echo "Invalid arguments"
